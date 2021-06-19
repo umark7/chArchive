e735367df921f615c80e7d0f68c41de63588707c
Camille Hall <73563880+camihall@users.noreply.github.com>
2021-06-19 13:22:50 -0500

    successfully fills in some challenges into excel
    It got several challenges correct, but some incorrect. Also, it didn't get all the challenges

--- govchallenges.py
+++ govchallenges.py
@@ -23,10 +23,7 @@ f.write(headers)
 archive_button = page_soup.body.find("ul",{"id":"primary-nav-5"})
 archive_accordion = archive_button.findAll("li",{"class":"usa-nav__submenu-item"})
 years = [govchal_link]
-for li in archive_accordion:
-    if li.a["href"][0] == '/':
-        years.append("https://www.challenge.gov" + li.a["href"])
-#print(years)
+print(years)
 
 #LOOP THROUGH EACH YEAR IN ARCHIVE
 for year_link in years:
@@ -38,14 +35,20 @@ for year_link in years:
 
     #HTML PARSING
     year_soup = soup(year_html, "html.parser")
-    #print(year_link)
+    print(year_link)
 
     #LOOP THROUGH LISTED CHALLENGES FOR EACH YEAR
     #find list of links
     chals = year_soup.findAll("div",{"class":["card__action card__action-block position-absolute", "card__action card__action-block"]})
+    print(chals)
     for chal in chals:
         #TO DO: for now, skip the ones that go to external links by checking if they start with https, if so, go to next iteration
-        chal_link = "https://www.challenge.gov" + chal.a["href"]
+        if chal.a["href"][0] == '/':
+            chal_link = "https://www.challenge.gov" + chal.a["href"]
+        else:
+            continue
+        
+        #chal_link = "https://www.challenge.gov" + chal.a["href"]
 
         #OPEN CONNECTION TO THAT CHALLENGE'S DETAILS
         chal_request = uReq(chal_link)
@@ -62,7 +65,7 @@ for year_link in years:
 
         #gGRAB DESCRIPTION
         chal_desc = chalpg_soup.main.find("h3", {"class":["challenge-tagline", "text-primary"]}).text
-        #print(chal_desc)
+        print(chal_desc)
 
         #GRAB TOTAL CASH PRIZE (in USD. parse only number, no $symbol)
         #QUESTION: should we acctually make this an int or leave it as a string?
@@ -73,14 +76,14 @@ for year_link in years:
         chal_funds = challenge_details[index_of_dollar+1: line1_end+1]
         #take comma out of number if present so it can be converted to integer
         chal_funds = chal_funds.replace(',','')
-        #print(chal_funds)
+        print(chal_funds)
 
         #GRAB SUBMISSION END
         start = challenge_details.find("Submission End: ")
         time_str = challenge_details[start+16:]#16 is the length of "Submission End:  "
         end = time_str.find(" ")
         chal_end_date = time_str[0:end]
-        #print(chal_end_date)
+        print(chal_end_date)
 
 #TO DO: something to streamline the code would be to make a function for parsing strings from challenge details
 #       (as done to get the submission date, cash prize, and agencies) so that there is less repeating/similar code.
@@ -101,7 +104,7 @@ for year_link in years:
         #concatenate
         chal_agencies = fed_agencies + "; " + nfed_agencies
         chal_agencies = chal_agencies.replace(',', ';')
-        #print(chal_agencies)
+        print(chal_agencies)
 
         #GRAB LINK TO CHALLENGE
         chal_link2 = chal_link
@@ -109,29 +112,6 @@ for year_link in years:
         if link_list != []:
             chal_link2 =chal_link2 + "; " + link_list[0]["href"] #decide which number is best to pull.
         print(chal_link2)
-
-
-        #how_to_enter_h3 = chalpg_soup.find("h3",{"id":"how-to-enter"})
-        #print(how_to_enter_h3)
-        #nexts = how_to_enter_h3.next_siblings
-        #print(nexts)
-        #chal_link2 = chal_link
-        #print(sib.name)
-        #while sib.name != 'h3':
-        #    link = sib.find("a")
-        #    print(link)
-        #    if (link != -1) and (link is not None):
-        #        chal_link2 = sib.a["href"]
-        #        print(chal_link)
-        #        break
-        #    sib = sib.next_element
-
-
-        #how_to_enter_section = chalpg_soup.find("h3",{"id":"how-to-enter"}).next_element.next_element.next_element
-        #if how_to_enter_section is not None:
-        #    chal_link2 = how_to_enter_section.a['href']
-        #else: chal_link2 = "NA"
-        #print(chal_link2)
 
         #TO DO: Grab subject (optimally more specific then "STEM")
 
